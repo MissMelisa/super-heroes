65045dc5dc8d9f27b7f6e46284ea1b047ff2cdfe
Melisa <melissarios497@gmail.com>
2021-08-26 15:50:57 -0300

    add login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,17 +1,45 @@
-import React from "react";
-import { Formik } from "formik";
-import {} from "bootstrap";
+import { useFormik } from "formik";
+import { Container, Button, Form } from "react-bootstrap";
 
-export default function Login() {
+function Login() {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
   return (
-    <div>
-      <input
-        type="text"
-        class="form-control"
-        placeholder="Username"
-        aria-label="Username"
-        aria-describedby="basic-addon1"
-      />
-    </div>
+    <Container>
+      <Form onSubmit={formik.handleSubmit}>
+        <Form.Group className="mb-3" controlId="email">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            onChange={formik.handleChange}
+            value={formik.values.email}
+          />
+          <Form.Text className="text-muted" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            onChange={formik.handleChange}
+            value={formik.values.password}
+          />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Container>
   );
 }
+export default Login;
