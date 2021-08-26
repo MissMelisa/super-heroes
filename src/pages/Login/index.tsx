16654d5a747a6059a384f1306e521ef6c20c2cbe
Rios Melisa <riosmelisa@Rioss-MacBook-Air.local>
2021-08-26 15:28:52 -0300

    add React Bootstrap

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Formik } from "formik";
+import {} from "bootstrap";
+
+export default function Login() {
+  return (
+    <div>
+      <input
+        type="text"
+        class="form-control"
+        placeholder="Username"
+        aria-label="Username"
+        aria-describedby="basic-addon1"
+      />
+    </div>
+  );
+}
