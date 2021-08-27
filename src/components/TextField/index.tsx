1f33c2dd9fdc1458671e02d80ef6e4059eee074c
Melisa <melissarios497@gmail.com>
2021-08-27 15:41:29 -0300

    add login's alert

--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -1,5 +1,5 @@
 import { Form } from "react-bootstrap";
-import "./styles.css";
+import styles from "./styles.module.css";
 
 type Props = {
   onChange: (e: React.ChangeEvent<any>) => void;
@@ -8,6 +8,7 @@ type Props = {
   label: string;
   placeholder: string;
   controlId: string;
+  error?: string;
 };
 export default function TextField({
   onChange,
@@ -16,18 +17,23 @@ export default function TextField({
   type,
   placeholder,
   controlId,
+  error,
 }: Props) {
   return (
-    <Form.Group className="textFieldContainer" controlId={controlId}>
-      <Form.Label className="textFieldLabel">{label}</Form.Label>
+    <Form.Group className={styles.textFieldContainer} controlId={controlId}>
+      <Form.Label className={styles.textFieldLabel}>{label}</Form.Label>
       <Form.Control
+        isInvalid={!!error}
         type={type}
         placeholder={placeholder}
         onChange={onChange}
         value={value}
         className="textField"
       />
-      <Form.Text />
+
+      <Form.Control.Feedback type="invalid" className={styles.errorMessage}>
+        {error}
+      </Form.Control.Feedback>
     </Form.Group>
   );
 }
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,8 +1,9 @@
 import axios from "axios";
 import { useFormik } from "formik";
-import { Container, Button, Form, Image } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { Container, Button, Form, Image, Alert } from "react-bootstrap";
 import TextField from "../../components/TextField";
-import "./styles.css";
+import styles from "./styles.module.css";
 
 type Credential = {
   email: string;
@@ -10,18 +11,20 @@ type Credential = {
 };
 
 function Login() {
+  const [authError, setAuthError] = useState<string>("");
+
   const validate = (values: Credential) => {
     const errors: Partial<Credential> = {};
 
     if (!values.email) {
-      errors.email = "Required";
+      errors.email = "Email is required";
     } else if (
       !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
     ) {
       errors.email = "Invalid email address";
     }
     if (!values.password) {
-      errors.password = "Required";
+      errors.password = "Password is required";
     }
     return errors;
   };
@@ -42,20 +45,34 @@ function Login() {
         })
         .catch(function (error) {
           console.log(error);
+          setAuthError(error.message);
         });
     },
   });
 
+  useEffect(() => {
+    if (authError) {
+      const interval = setInterval(() => {
+        setAuthError("");
+      }, 5000);
+
+      return () => {
+        clearInterval(interval);
+      };
+    }
+  }, [authError]);
+
   return (
-    <Container fluid="md">
+    <Container fluid="md" className={styles.containerLogin}>
       <Image
         alt="Alkemy"
         fluid
-        className="imageLogin"
-        src="https://media-exp1.licdn.com/dms/image/C4E1BAQEDDjuh9HQchg/company-background_10000/0/1610631110628?e=2159024400&v=beta&t=00JMFny1Y6JiSd8rpPDIfJ_6vNH6NhtCK_yban1zy3c"
+        className={styles.imageLogin}
+        src="images/alkemy.jpeg"
       />
-      <h2 className="loginTitle">Login</h2>
-      <Form onSubmit={formik.handleSubmit} className="formContainer">
+      <h2 className={styles.loginTitle}>Login</h2>
+
+      <Form onSubmit={formik.handleSubmit} className={styles.formContainer}>
         <TextField
           onChange={formik.handleChange}
           value={formik.values.email}
@@ -63,6 +80,7 @@ function Login() {
           type="email"
           label="Email"
           placeholder="email"
+          error={formik.errors.email}
         />
         <TextField
           type="password"
@@ -71,12 +89,24 @@ function Login() {
           placeholder="Password"
           onChange={formik.handleChange}
           value={formik.values.password}
+          error={formik.errors.password}
         />
 
-        <Button type="submit" variant="info" className="buttonLogin">
+        <Button type="submit" variant="info" className={styles.buttonLogin}>
           Enviar
         </Button>
       </Form>
+      {authError && (
+        <Alert
+          className={styles.alertContainer}
+          variant="danger"
+          onClose={() => setAuthError("")}
+          dismissible
+        >
+          <Alert.Heading>Oh! You got an error!</Alert.Heading>
+          <p className={styles.alertStory}>{authError}</p>
+        </Alert>
+      )}
     </Container>
   );
 }
